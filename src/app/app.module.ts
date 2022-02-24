79431a50fb22286f8cb908ce4cc9635a6cb682fb
ahmedfawzi1 <ahmed.fawzii6102000@gmail.com>
2022-02-24 23:38:48 +0200

    Flexor

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { Section1Component } from './section1/section1.component';
+import { Section2Component } from './section2/section2.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    NavbarComponent,
+    Section1Component,
+    Section2Component
   ],
   imports: [
     BrowserModule,
